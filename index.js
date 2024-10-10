e4e04c2e6091b511ae0ea9abe303e1fe4c6c0f70
louis-ngugi <louisngugi858@gmail.com>
2024-10-10 18:51:06 +0300

    complete index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+//Question 1
+// Initial employee object
+let employee = {};
+
+// Function to update employee with a new key and value (non-destructive)
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+//Question 2
+// Function to destructively update employee with a new key and value
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+//Question 3
+// Function to delete a key from a clone of employee (non-destructive)
+function deleteFromEmployeeByKey(employee, key) {
+  const { [key]: omitted, ...newEmployee } = employee;
+  return newEmployee;
+}
+
+//Question 4
+// Function to destructively delete a key from employee
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
